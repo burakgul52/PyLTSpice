96818d483ecd7ebc317a9b6dfda27b71ecd27546
burakgul52 <burakgul5225@gmail.com>
2021-02-15 21:26:18 +0300

    burak_python
    odu mu acep

---
+++ venv/Scripts/__init__.py
@@ -0,0 +1,7 @@
+#!C:\Users\Admin\Downloads\PyLTSpice\venv\Scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'PyLTSpice==1.1','__init__.py'
+__requires__ = 'PyLTSpice==1.1'
+__import__('pkg_resources').require('PyLTSpice==1.1')
+__file__ = 'C:\\Users\\Admin\\Downloads\\PyLTSpice\\PyLTSpice\\__init__.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
